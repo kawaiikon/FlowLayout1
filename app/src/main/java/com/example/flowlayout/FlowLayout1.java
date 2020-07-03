f51cc15f5346a8422c36bd82c336d2ea8299d536
bianjiacheng@ydfinance.com.cn <123bian456>
2020-07-03 17:24:20 +0800

    度量完了孩子

--- app/src/main/java/com/example/flowlayout/FlowLayout1.java
+++ app/src/main/java/com/example/flowlayout/FlowLayout1.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -28,7 +29,74 @@ public class FlowLayout1 extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        //先度量孩子
+        for(int i = 0; i < getChildCount(); i++) {
+            View childView = getChildAt(i);
+
+            LayoutParams layoutParams = childView.getLayoutParams();
+            int childWidthMeasureSpace = getChildMeasureSpace(widthMeasureSpec, getPaddingLeft() + getPaddingRight(), layoutParams.width);
+            int childHeightMeasureSpace = getChildMeasureSpace(heightMeasureSpec, getPaddingTop() + getPaddingBottom(), layoutParams.height);
+            childView.measure(childWidthMeasureSpace, childHeightMeasureSpace);
+        }
+
+
+
+
+
+
+
+
+        //度量自己
+//        setMeasuredDimension();
+    }
+
+    private int getChildMeasureSpace(int measureSpace, int padding, int childDimension) {
+        int parentWidthSize = Math.max(0, MeasureSpec.getSize(measureSpace) - padding);
+        int childMode = 0;
+        int childSize = 0;
+        switch (MeasureSpec.getMode(measureSpace)) {
+            case MeasureSpec.EXACTLY:
+                if (childDimension == LayoutParams.MATCH_PARENT) {
+                    childMode = MeasureSpec.EXACTLY;
+                    childSize = parentWidthSize;
+                }
+                if (childDimension == LayoutParams.WRAP_CONTENT) {
+                    childMode = MeasureSpec.EXACTLY;
+                    childSize = parentWidthSize;
+                }
+                if (childDimension >= 0) {
+                    childMode = MeasureSpec.EXACTLY;
+                    childSize = childDimension;
+                }
+            case MeasureSpec.AT_MOST:
+                if (childDimension == LayoutParams.MATCH_PARENT) {
+                    childMode = MeasureSpec.AT_MOST;
+                    childSize = parentWidthSize;
+                }
+                if (childDimension == LayoutParams.WRAP_CONTENT) {
+                    childMode = MeasureSpec.AT_MOST;
+                    childSize = parentWidthSize;
+                }
+                if (childDimension >= 0) {
+                    childMode = MeasureSpec.EXACTLY;
+                    childSize = childDimension;
+                }
+            case MeasureSpec.UNSPECIFIED:
+                if (childDimension == LayoutParams.MATCH_PARENT) {
+                    childMode = MeasureSpec.UNSPECIFIED;
+                    childSize = 0;
+                }
+                if (childDimension == LayoutParams.WRAP_CONTENT) {
+                    childMode = MeasureSpec.UNSPECIFIED;
+                    childSize = 0;
+                }
+                if (childDimension >= 0) {
+                    childMode = MeasureSpec.EXACTLY;
+                    childSize = childDimension;
+                }
+        }
+        return MeasureSpec.makeMeasureSpec(childSize, childMode);
     }
 
     @Override
